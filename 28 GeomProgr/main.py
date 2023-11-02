7dcf907f44bc902aa054871c08f6114b39366568
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-11-02 15:45:06 +0300

    Fix 28-29 problem

--- 28 GeomProgr/main.py
+++ 28 GeomProgr/main.py
@@ -1,13 +1,16 @@
-from itertools import tee, islice
+from itertools import islice
 class Geom:
+    @staticmethod
+    def iter(b, q):
+        i = b
+        while True:
+            yield i
+            i *= q
+    
     def __init__(self, b, q):
-        def iter(b, q):
-            i = b
-            while True:
-                yield i
-                i *= q
-        self.iter = iter(b, q)
-        self.sec_iter = iter(b, q)
+        self.b = b
+        self.q = q
+        self.iter = Geom.iter(b, q)
     
     def __iter__(self):
         return self.iter
@@ -15,19 +18,31 @@ class Geom:
     def __next__(self):
         return next(self.iter)
     
-    def __getitem__(self, slice):
-        self.sec_iter, buf = tee(self.sec_iter)
-        if slice is Ellipsis:
-            return buf
-        if isinstance(slice, tuple) and slice[1] is Ellipsis:
-            return islice(buf, slice[0], slice[2])
-        if slice.start is None and slice.stop is None and slice.step < 0:
+    def __getitem__(self, args):
+        cur_iter = Geom.iter(self.b, self.q)
+        if args is Ellipsis:
+            return cur_iter
+
+        if isinstance(args, tuple):
+            if len(args) == 3:
+                args = (args[0], args[2], None)
+            elif args[0] is Ellipsis:
+                args = (None, args[1], None)
+            elif args[1] is Ellipsis:
+                args = (args[0], None, None)
+        else:
+            args = (args.start, args.stop, args.step)
+
+        if args[0] is args[1] is args[2] is None:
+            return cur_iter
+        if args[0] is args[1] is None and args[2] < 0:
             return None
-        if slice.step is not None and slice.step < 0:
-            stop = slice.stop
-            if stop is None:
-                stop = 0
-            return (i for i in [next(buf) for j in range(stop, slice.start + 1)][::slice.step])
-        return islice(buf, slice.start, slice.stop, slice.step)
+        if args[2] is not None and args[2] < 0:
+            args = (args[1], args[0], args[2])
+            if args[0] is None:
+                args = (0, args[1], args[2])
+            return list(islice(cur_iter, args[0], args[1] + 1))[::args[2]]
+        return islice(cur_iter, *args)
+
 
 
--- 29 SmileClass/main.py
+++ 29 SmileClass/main.py
@@ -1,17 +1,18 @@
 class Smile:
     def __init__(self, n):
         self.n = n
-        if n == 0:
-            self.val = ""
-        elif abs(n) == 1:
-            self.val = '/1\\\n|"|\n\\-/'
-        elif abs(n) > 1:
-            rev = n < 0
-            n = abs(n)
-            height = n
-            width = n*2 - 1
-            d = n // 4
-            first_row = f"/{str(n)}{'-' * (width - len(str(n)))}\\"
+    def __str__(self):
+        if self.n == 0:
+            val = ""
+        elif abs(self.n) == 1:
+            val = '/1\\\n|"|\n\\-/'
+        elif abs(self.n) > 1:
+            rev = self.n < 0
+            absn = abs(self.n)
+            height = absn
+            width = absn*2 - 1
+            d = absn // 4
+            first_row = f"/{str(absn)}{'-' * (width - len(str(absn)))}\\"
             matr = [f"|{' ' * width}|" for i in range(height)]
             matr[d] = f"|{' '*d}O{' '*(width - 2*(d + 1))}O{' '*d}|"
             matr[-d - 1] = f"|{' '*d} {'-'*(width - 2*(d + 1))} {' '*d}|"
@@ -20,9 +21,8 @@ class Smile:
             matr.insert(0, first_row)
             last_row = f"\\{'-' * width}/"
             matr.append(last_row)
-            self.val = '\n'.join(matr)
-    def __str__(self):
-        return self.val
+            val = '\n'.join(matr)
+        return val
     def __neg__(self):
         return Smile(-self.n)
     def __abs__(self):
@@ -32,10 +32,8 @@ class Smile:
     __radd__ = __add__
     def __sub__(self, other):
         return Smile(self.n - other.n)
-        pass
     __rsub__ = __sub__
     def __mul__(self, other):
         return Smile(self.n * other)
     __rmul__ = __mul__
-
 
